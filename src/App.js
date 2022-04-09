cf7f660d7692f28b019c64b51e72174d0ef44110
ZukaBri3k <kyrilldumerchat@gmail.com>
2022-04-09 21:16:40 +0200

    test 	.firebase/ 	.firebaserc 	firebase.json 	firestore.indexes.json 	firestore.rules 	src/components/ 	src/context/ 	src/firebase-config.js 	src/pages/ 	src/video/

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import SignUp from "./pages/SignUp.js";
 import SignIn from "./pages/SignIn.js";
 import Private from "./pages/Private/Private.js";
 import AccountHome from "./pages/Private/AccountHome/AccountHome.js";
-import NavBar from "./components/NavBar";
+import ModifyMyAccount from "./pages/ModifyMyAccount.js";
+import Redirect from "./pages/Redirect.js";
+import Research from "./pages/Private/research/Research.js";
 
 function App() {
   return (
@@ -13,8 +15,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/redirect" element={<Redirect />} />
         <Route path="/private" element={<Private />}>
           <Route path="/private/account" element={<AccountHome />} />
+          <Route path="/private/account/modify" element={<ModifyMyAccount />} />
+          <Route path="/private/research" element={<Research />} />
         </Route>
       </Routes>
     </>
